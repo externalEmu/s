fa6e8628f504c30928cdce276d457daf3741e1ea
hipeopeo <hipeopeo@gmail.com>
2019-01-02 15:33:15 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
 del yuzu-keys-installer.bat
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://s.hipeopeodev.us/yuzu-keys-installer.bat', 'yuzu-keys-installer.bat')
+powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/hipeopeo/s/master/yuzu-keys-installer.bat', 'yuzu-keys-installer.bat')
 yuzu-keys-installer.bat
 exit
